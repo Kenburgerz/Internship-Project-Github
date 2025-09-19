b2a78a2d754e63234d0f4db0154fa858d80d8c6c
Kendrick <kenburger2807@gmail.com>
2025-09-19 10:12:31 +0800

    Add files via upload

---
+++ Webpage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Box = () => {
+  return (
+    <div className="w-[516px] h-[328px]">
+      <div className="fixed top-[265px] left-[330px] w-[516px] h-[328px] bg-[#6c0707]" />
+    </div>
+  );
+};
